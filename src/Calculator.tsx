16fbb78d205fe323988a5e239f214468b75182e6
HeIIJoke <mamedovelnur2024@mail.ru>
2025-03-29 08:59:15 +0300

    Добавил новые изменения

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,59 +1,64 @@
 import { useState } from "react";
+import styles from "./calculator.module.css";
 
 const Calculator = () => {
-  const operations: { label: string; value: string }[][] = [
+  const operations: { label: string; value: string; className: string }[][] = [
     [
-      { label: "xʸ", value: "^" },
-      { label: "lg", value: "lg" },
-      { label: "ln", value: "ln" },
-      { label: "1/x", value: "1/x" },
-      { label: "π", value: "pi" },
+      { label: "xʸ", value: "^", className: styles.anotheroperations },
+      { label: "lg", value: "lg", className: styles.anotheroperations },
+      { label: "ln", value: "ln", className: styles.anotheroperations },
+      { label: "1/x", value: "1/x", className: styles.anotheroperations },
+      { label: "π", value: "pi", className: styles.anotheroperations },
     ],
     [
-      { label: "√", value: "sqrt" },
-      { label: "C", value: "C" },
-      { label: "⌫", value: "del" },
-      { label: "%", value: "%" },
-      { label: "÷", value: "/" },
+      { label: "√", value: "sqrt", className: styles.anotheroperations },
+      { label: "C", value: "C", className: styles.operations },
+      { label: "⌫", value: "del", className: styles.operations },
+      { label: "%", value: "%", className: styles.operations },
+      { label: "÷", value: "/", className: styles.operations },
     ],
     [
-      { label: "x!", value: "factorial" },
-      { label: "7", value: "7" },
-      { label: "8", value: "8" },
-      { label: "9", value: "9" },
-      { label: "x", value: "*" },
+      { label: "x!", value: "factorial", className: styles.anotheroperations },
+      { label: "7", value: "7", className: styles.numbers },
+      { label: "8", value: "8", className: styles.numbers },
+      { label: "9", value: "9", className: styles.numbers },
+      { label: "x", value: "*", className: styles.operations },
     ],
     [
-      { label: "sin", value: "sin" },
-      { label: "4", value: "4" },
-      { label: "5", value: "5" },
-      { label: "6", value: "6" },
-      { label: "-", value: "-" },
+      { label: "sin", value: "sin", className: styles.anotheroperations },
+      { label: "4", value: "4", className: styles.numbers },
+      { label: "5", value: "5", className: styles.numbers },
+      { label: "6", value: "6", className: styles.numbers },
+      { label: "-", value: "-", className: styles.operations },
     ],
     [
-      { label: "cos", value: "cos" },
-      { label: "1", value: "1" },
-      { label: "2", value: "2" },
-      { label: "3", value: "3" },
-      { label: "+", value: "+" },
+      { label: "cos", value: "cos", className: styles.anotheroperations },
+      { label: "1", value: "1", className: styles.numbers },
+      { label: "2", value: "2", className: styles.numbers },
+      { label: "3", value: "3", className: styles.numbers },
+      { label: "+", value: "+", className: styles.operations },
     ],
     [
-      { label: "tan", value: "tan" },
-      { label: "e", value: "exponenta" },
-      { label: "0", value: "0" },
-      { label: ".", value: "." },
-      { label: "=", value: "=" },
+      { label: "tan", value: "tan", className: styles.anotheroperations },
+      { label: "e", value: "exponenta", className: styles.numbers },
+      { label: "0", value: "0", className: styles.numbers },
+      { label: ".", value: ".", className: styles.numbers },
+      { label: "=", value: "=", className: styles.result },
     ],
   ];
 
   return (
-    <div>
-      <div>{"0"}</div>
-      <div>
+    <div className={styles.calculator}>
+      <div className={styles.display}>{"0"}</div>
+      <div className={styles.buttons}>
         {operations.map((row, rowIndex) => (
           <div key={rowIndex} className="buttons-row">
             {row.map((operation) => (
-              <button key={operation.value} onClick={() => {}}>
+              <button
+                key={operation.value}
+                onClick={() => {}}
+                className={operation.className}
+              >
                 {operation.label}
               </button>
             ))}
